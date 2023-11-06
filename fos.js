ebf34469b5a2b5d076de0374f9326ee01fcae8e9
Laura Paredes <lauraparedesl35@outlook.com>
2023-11-06 17:43:12 -0500

    ya casi termino

---
+++ fos.js
@@ -0,0 +1,125 @@
+//EJERCICIO 1
+
+ let mostrar=mensaje => console.log(mensaje)
+
+ //EJERCICIO 2
+
+ let crearMultiplicacion=(numero1, numero2) => {
+    let resultado= numero1 * numero2
+    return resultado
+ }
+
+console.log(crearMultiplicacion(2,2))
+
+//EJERCICIO 3
+
+const array=[1,2,3,4,5,6,7,8,9]
+
+let nuevoArray=array.map ((number, number2) => crearMultiplicacion(number, number2))
+    console.log(nuevoArray)
+
+//EJERCICIO 4
+
+let cervezasMasAlcoholicas= beers.filter (beers => {return beers.abv > 7.3})
+console.log(cervezasMasAlcoholicas)
+
+//EJERCICIO 5
+
+let cervezasMenosAmargas= beers.filter (beers => {return beers.ibu < 42})
+console.log(cervezasMenosAmargas)
+
+//EJERCICIO 6
+
+function nombreCerveza(nombre){return beers.find(beers=> beers.name==nombre)}
+console.log(nombreCerveza("Buzz"))
+
+//EJERCICIO 7
+
+let valorIngresado= 60
+function encontarIbu(beers,valor){ 
+    const comparar=(beers.find(beer=> beer.ibu==valor))
+ if(comparar){
+return comparar
+}
+console.log(`No existe cerveza con ibu de ${valor}` )
+
+}
+console.log(encontarIbu(beers,valorIngresado))
+
+//EJERCICIO 8
+
+function buscarPosicion (nombre){
+    let encontrar=nombreCerveza(nombre)
+    if(encontrar)
+    return beers.indexOf(encontrar)
+    else{
+    console.log(`${nombre} no existe`)
+    }
+    }
+    console.log(buscarPosicion("Trashy Blonde"))
+
+//EJERCICIO 9
+
+function nuevoArrayCervezas (objeto , valor) {
+    let arrayNuevo = [ ]
+    for (let i = 0; i < objeto.length; i++) {
+        const retornoFuncion = valor( objeto[i], i , objeto)
+        arrayNuevo.push(retornoFuncion)
+    }
+    return arrayNuevo
+}
+
+function encontrarAbv(beers,valor){ 
+    const noExcedenAbv=(beers.filter(beer=> beer.abv <= valor))
+    .map((beer)=> {
+        const cervezaNueva={
+            nombre: beer.name,
+            abv: beer.abv,
+            ibu: beer.ibu,
+        }
+        return cervezaNueva
+    } )
+    return noExcedenAbv
+} 
+console.log(encontrarAbv(beers , 4.5))
+
+//EJERCICIO 10
+
+function cervezasNuevas(arreglo , propiedad , booleano){
+    const cervezasOrdenadas=arreglo.toSorted((a,b) => {
+        if( a[propiedad] < b[propiedad] ){
+            return -1
+        }
+        if( a[propiedad] > b[propiedad]){
+            return 1
+        }
+        
+        return booleano
+    } 
+    ).map(arreglo => arreglo.name)
+   
+    return cervezasOrdenadas.slice (0 , 10)
+    
+}
+
+console.log(cervezasNuevas(beers , "name" , false))
+
+//EJERCICIO 11
+function crearTabla(array, id){
+    const tabla=document.getElementById("tbody")
+
+    const columnas=document.createElement("tr")
+
+    for (const elementos of array) {
+        if (elementos.name) {
+            let columna1=document.
+            
+        }
+    }
+
+    
+
+
+
+}
+
